6065e7f2315ff81279412459a548486108cbcb4c
GMSyrimis <gmsyrimis@gmail.com>
2015-03-17 17:25:59 -0400

    first commit, pretty much done

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -10,22 +10,34 @@ import java.util.Scanner;
 public class VerbosityCalculator {
     public static void main(String[] args){
 
-        // An abstract representation of a File
-        File file = new File("PATH to text file");
+       try{
+           //Load file into String
+           String tales = new Scanner(new File("/Users/c4q-george/IdeaProjects/VerbosityCalculator/tales.txt")).useDelimiter("\\Z").next();
+           //Convert string to string array, easy way to count tokens split by a space
+           String[] stringArray = tales.split(" ");
+           //getting the size of the array and indirectly the number of words
+           int wordCount = stringArray.length;
+           //period count initialization
+           int periodCount=0;
 
-        try{
-            Scanner sc = new Scanner(file);
-            /**
-             *  Your CODE goes here
-             *
-             *
-             *
-             *
-             */
+           // Loop through array and look for periods in the contents.
+           // This can produce artifacts but unless I build special exceptions for Mr. Mrs. etc.
+           for(int i = 0; i<wordCount; i++){
+               if (stringArray[i].contains(".")){
+                   periodCount++;
+               }
+           }
+
+           double verbocity = wordCount/periodCount;
+
+           System.out.println(verbocity);
+
 
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
+
+
     }
 }
