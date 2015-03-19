d1380d134d009582107b3f07c2682348090d4e5f
GMSyrimis <gmsyrimis@gmail.com>
2015-03-18 21:58:52 -0400

    Finished

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -2,35 +2,40 @@ package nyc.c4q.ac21;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
-    public static void main(String[] args){
 
-       try{
-           //Load file into String
-           String tales = new Scanner(new File("/Users/c4q-george/IdeaProjects/VerbosityCalculator/tales.txt")).useDelimiter("\\Z").next();
-           //Convert string to string array, easy way to count tokens split by a space
-           String[] stringArray = tales.split(" ");
-           //getting the size of the array and indirectly the number of words
-           int wordCount = stringArray.length;
-           //period count initialization
-           int periodCount=0;
+    public static void verbosityCalc(String filePATH){
+        double verbosity=0;
 
-           // Loop through array and look for periods in the contents.
-           // This can produce artifacts but unless I build special exceptions for Mr. Mrs. etc.
-           for(int i = 0; i<wordCount; i++){
-               if (stringArray[i].contains(".")){
-                   periodCount++;
-               }
-           }
+        try{
+            //Load file into String
+            String tales = new Scanner(new File(filePATH)).useDelimiter("\\Z").next();
+            //Convert string to string array, easy way to count tokens split by a space
+            String[] stringArray = tales.split(" ");
+            //getting the size of the array and indirectly the number of words
+            int wordCount = stringArray.length;
+            //period count initialization
+            int periodCount=0;
 
-           double verbocity = wordCount/periodCount;
+            // Loop through array and look for periods in the contents.
+            // This can produce artifacts but unless I build special exceptions for Mr. Mrs. etc.
+            for(int i = 0; i<wordCount; i++){
+                if (stringArray[i].contains(".")){
+                    periodCount++;
+                }
+            }
+            double dWordCount = (double)wordCount;
+            double dperiodCount= (double)periodCount;
 
-           System.out.println(verbocity);
+            verbosity = dWordCount/dperiodCount;
+            String deciVerb = Double.toString(verbosity).substring(0,5);
+            System.out.println(deciVerb);
 
 
         }
@@ -40,4 +45,10 @@ public class VerbosityCalculator {
 
 
     }
+
+    public static void main(String[] args){
+
+        verbosityCalc("/Users/c4q-george/ideaprojects/verbositycalculator/moby.txt");
+    }
+
 }
